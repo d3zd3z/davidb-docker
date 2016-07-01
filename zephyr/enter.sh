26cd1e86b108ebe6263b90e02df0d977fd20e848
David Brown <david.brown@linaro.org>
2016-07-01 16:47:42 -0600

    Initial zephyr image
    This image follows the instructions to setup an environment and
    install the Zephyr SDK into an Ubuntu 14.04 image.

---
+++ zephyr/enter.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# Start the 'asme' container to run as the current user in the current
+# directory.
+
+# The bind volumes should probably be changed to be broader than just
+# the current example, but this case does work.
+#
+# You may want to bind your home directory, or at least things like
+# ~/.ssh:~/.ssh:ro
+
+container=davidb/zephyr:latest
+
+docker run --rm -ti \
+	-v "$(pwd):$(pwd)" \
+	-v "/mnt/linaro:/mnt/linaro" \
+	-v "/mnt/zephyr:/mnt/zephyr" \
+	-v "$HOME/.bashrc:$HOME/.bashrc" \
+	-v "$HOME/.bash_profile:$HOME/.bash_profile" \
+	-v "$HOME:$HOME" \
+	-v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK:ro \
+	-e USER=$USER \
+	-e HOME=$HOME \
+	-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+        -e UID=$(id -u) \
+        -e GID=$(id -g) \
+	-w $(pwd) \
+	$container \
+	bash -l \
+        "$@"
