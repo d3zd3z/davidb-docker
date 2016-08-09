9463296caf2bbee0331eb518778df8942166a194
David Brown <david.brown@linaro.org>
2016-08-09 14:28:18 -0600

    Build environment for building op-tee.

---
+++ op-tee/enter.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# Start the 'asme' container to run as the current user in the current
+# directory.
+
+# The bind volumes should probably be changed to be broader than just
+# the current example, but this case does work.
+#
+# You may want to bind your home directory, or at least things like
+# ~/.ssh:~/.ssh:ro
+
+container=davidb/op-tee:latest
+
+docker run --rm -ti \
+	-v "$(pwd):$(pwd)" \
+	-v "$HOME/.bashrc:$HOME/.bashrc" \
+	-v "$HOME/.bash_profile:$HOME/.bash_profile" \
+	-v "/mnt/linaro:/mnt/linaro" \
+	-v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK:ro \
+	-e USER=$USER \
+	-e HOME=$HOME \
+	-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+        -e UID=$(id -u) \
+        -e GID=$(id -g) \
+	-w $(pwd) \
+	$container \
+	bash -l \
+        "$@"
